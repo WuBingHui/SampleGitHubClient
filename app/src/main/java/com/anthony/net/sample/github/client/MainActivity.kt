9594101f845fecc32c564ad88646d60a8e118973
MSI\Anthony <a173200488@gmail.com>
2022-12-08 11:34:34 +0800

    init

---
+++ app/src/main/java/com/anthony/net/sample/github/client/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.anthony.net.sample.github.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
